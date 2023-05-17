06859468ceb0692f99759d6e23f99fe135008745
Manu Dawber <manu.dawber@h2gopower.com>
2023-05-17 16:47:10 +0100

    feat: enable last name parameter

--- src/main.py
+++ src/main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
+def print_hi(first_name: str, last_name: str = None) -> None:
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {first_name}{f" {last_name}" if last_name else None}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Manu', 'Dawber')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
